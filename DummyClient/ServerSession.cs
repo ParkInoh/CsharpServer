c5e0dd4d3f252b44adc3a0a7651da57ef19e4b0c
InohPark <inohzzang@gmail.com>
2022-09-15 22:16:59 +0900

    패킷 직렬화 개선
    파싱하기 전에 범위를 판단하도록 개선

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -1,21 +1,67 @@
 ﻿using ServerCore;
+using System;
 using System.Net;
 using System.Text;
 
 namespace DummyClient {
-    class Packet {
+    public abstract class Packet {
+        // 패킷의 사이즈와 Id가 굳이 필요한가?
+        // 생성자에서 할당하는데, TryWriteBytes 에서 직접 넣으면?
+        // size도 패킷세션이 참고만 함
         public ushort size;
         public ushort packetId;
+
+        // 버퍼에 쓰기/읽기 수행 부분을 분리
+        public abstract void Read(ArraySegment<byte> seg);
+        public abstract ArraySegment<byte> Write();
     }
 
     // 예제 패킷
     class PlayerInfoReq : Packet {
-        public long playerid;
-    }
+        public long playerId;
 
-    class PlayerInfoOk : Packet {
-        public int hp;
-        public int attack;
+        public PlayerInfoReq () {
+            packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> seg) {
+            ushort count = 0;
+
+            // Read 함수로 들어왔다는 것은 size, packetId를 이미 추출했다는 것
+            //ushort size = BitConverter.ToUInt16(segment.Array, segment.Offset);
+            count += 2;
+            //ushort packetId = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+            count += 2;
+            // 파싱 시작 위치가 packetId 다음이므로 + count 해준다.
+            // 바로 변환하는 것이 아니라, 충분한 공간이 있는지 확인해야 한다.
+            // ReadOnlySpan은, 지정한 범위를 집어준다. 범위가 벗어나면 예외가 처리될 것이다.
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(seg.Array, seg.Offset + count, seg.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write() {
+            ArraySegment<byte> seg = SendBufferHelper.Open(4096);
+            // 한번이라도 실패하면 false가 되도록 success 변수를 & 연산한다.
+            ushort count = 0;
+            bool success = true;
+
+            // 이전의 바이트 배열 세 줄은 새로 만든다. 대신 segment에 바로 넣어주면 어떨지?
+            // BitConverter.TryWriteBytes를 쓸 수 있다.
+            // 쓸 위치의 오프셋이 계속 바뀌어야 한다. 또한 버퍼의 크기도 그만큼 줄어야 한다.
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset, segment.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset, seg.Count), count);
+
+            if (success == false) {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);  // 12 = 2 + 2 + 8
+        }
     }
 
     // 패킷의ID 를 구분할 수 있어야 함
@@ -32,36 +78,14 @@ namespace DummyClient {
 
             // 직렬화: 메모리 상의 인스턴스를 버퍼에 넣는 작업
 
-            PlayerInfoReq packet = new PlayerInfoReq() { 
-                //size = ,  // 사이즈는 계산이 끝난 후에 알 수 있다.
-                packetId = (ushort) PacketID.PlayerInfoReq,
-                playerid = 1001
-            };
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
             // 보내기
-            // 자동화하고 싶음
-            // 몇 바이트를 넣었는지 추적하는 변수 count를 둔다.
+            // Write 함수로 처리됨
+            ArraySegment<byte> seg = packet.Write();
 
-            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
-            // 한번이라도 실패하면 false가 되도록 success 변수를 & 연산한다.
-            ushort count = 0;
-            bool success = true;
-
-            // 이전의 바이트 배열 세 줄은 새로 만든다. 대신 segment에 바로 넣어주면 어떨지?
-            // BitConverter.TryWriteBytes를 쓸 수 있다.
-            // 쓸 위치의 오프셋이 계속 바뀌어야 한다. 또한 버퍼의 크기도 그만큼 줄어야 한다.
-            //success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset, segment.Count), packet.size);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset + count, segment.Count - count), packet.packetId);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset + count, segment.Count - count), packet.playerid);
-            count += 8;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset, segment.Count), count);
-
-            ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);  // 12 = 2 + 2 + 8
-
-            if (success) {
-                Send(sendBuff);
+            if (seg != null) {
+                Send(seg);
             }
         }
 
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -2,19 +2,60 @@
 using System.Net;
 
 namespace Server {
-    class Packet {
+    public abstract class Packet {
         public ushort size;
         public ushort packetId;
+
+        // 버퍼에 쓰기/읽기 수행 부분을 분리
+        public abstract void Read(ArraySegment<byte> seg);
+        public abstract ArraySegment<byte> Write();
     }
 
     // 예제 패킷
     class PlayerInfoReq : Packet {
-        public long playerid;
-    }
+        public long playerId;
 
-    class PlayerInfoOk : Packet {
-        public int hp;
-        public int attack;
+        public PlayerInfoReq() {
+            packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> seg) {
+            ushort count = 0;
+
+            // Read 함수로 들어왔다는 것은 size, packetId를 이미 추출했다는 것
+            //ushort size = BitConverter.ToUInt16(segment.Array, segment.Offset);
+            count += 2;
+            //ushort packetId = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+            count += 2;
+
+            // 파싱 시작 위치가 packetId 다음이므로 + count 해준다.
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(seg.Array, seg.Offset + count, seg.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write() {
+            ArraySegment<byte> seg = SendBufferHelper.Open(4096);
+            // 한번이라도 실패하면 false가 되도록 success 변수를 & 연산한다.
+            ushort count = 0;
+            bool success = true;
+
+            // 이전의 바이트 배열 세 줄은 새로 만든다. 대신 segment에 바로 넣어주면 어떨지?
+            // BitConverter.TryWriteBytes를 쓸 수 있다.
+            // 쓸 위치의 오프셋이 계속 바뀌어야 한다. 또한 버퍼의 크기도 그만큼 줄어야 한다.
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(segment.Array, segment.Offset, segment.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset + count, seg.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(seg.Array, seg.Offset, seg.Count), count);
+
+            if (success == false) {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);  // 12 = 2 + 2 + 8
+        }
     }
 
     // 패킷의ID 를 구분할 수 있어야 함
@@ -52,6 +93,9 @@ namespace Server {
             // ServerSession과 마찬가지로 count 변수를 둔다.
             ushort count = 0;
 
+            // size가 실제 패킷 정보와 일치하는지 판별하기 어렵다.
+            // size를 임의로 조작하여 보내면 읽을 때 조작된 값이 읽힌다.
+            // 즉 값을 믿지 말고 참고만 해야 한다.
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
             count += 2;
             ushort packetId = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
@@ -60,10 +104,10 @@ namespace Server {
             // id의 enum에 따라 처리를 달리 한다.
             switch ((PacketID) packetId) {
                 case PacketID.PlayerInfoReq:
-                    // 파싱 시작 위치가 packetId 다음이므로 + count 해준다.
-                    long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
-                    count += 8;
-                    Console.WriteLine($"PlayerInfoReq - playerId: {playerId}");
+                    // 인스턴스를 생성하고 버퍼를 읽음
+                    PlayerInfoReq req = new PlayerInfoReq();
+                    req.Read(buffer);
+                    Console.WriteLine($"PlayerInfoReq - playerId: {req.playerId}");
                     break;
             }
 
